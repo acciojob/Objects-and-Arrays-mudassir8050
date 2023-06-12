4802604a8c2feac6cf9abebfafb2e5c8bc47899f
mudassir8050 <125982472+mudassir8050@users.noreply.github.com>
2023-06-12 11:21:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,16 +2,13 @@ var players = ["John", "Bob", "Alice", "Poppy"];
 
 var person = {
   name: "John Doe",
-  age: 80,
-	
+  age: 80,	
 };
-
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
-var team=players;
-var team1=players;
-var cap1=person;
-window.players = players;
-window.person = person;
-window.team = team;
-window.team1 = team1;
-window.cap1 = cap1;
+var team = window.players;
+var team1 = window.players;
+var cap1 = window.person;
+// window.players = players;
+// window.person = person;
+// window.team = team;
+// window.team1 = team1;
+// window.cap1 = cap1;
