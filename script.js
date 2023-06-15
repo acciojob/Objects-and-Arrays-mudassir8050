1ee29c9c4ceb9cd06dce2cda34a39263579b700c
mudassir8050 <125982472+mudassir8050@users.noreply.github.com>
2023-06-15 13:00:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-# Given array and object
+// # Given array and object
 players = ["John", "Mike", "Sarah"]
 person = {"name": "Alice", "age": 25}
 
-# Create a reference to players array
+// # Create a reference to players array
 team = players
 
-# Create a copy of players array
+// # Create a copy of players array
 team1 = players.copy()
 
-# Create a copy of person object
+// # Create a copy of person object
 cap1 = person.copy()
 
-# Print the variables
+// # Print the variables
 print("team:", team)
 print("team1:", team1)
 print("cap1:", cap1)
