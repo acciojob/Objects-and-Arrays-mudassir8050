a3b2ea84461a74fccaacf6bcf4b30165f445d23d
mudassir8050 <125982472+mudassir8050@users.noreply.github.com>
2023-06-15 13:09:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ var person = {
   name: "John Doe",
   age: 80,	
 };
-var team = players
-var team1 = players.copy()
-var cap1 = person.copy()
-window.players = players;
-window.person = person;
-window.team = team;
-window.team1 = team1;
-window.cap1 = cap1;
+var team = #players
+var team1 = window.player
+var cap1 = window.person
+// window.players = players;
+// window.person = person;
+// window.team = team;
+// window.team1 = team1;
+// window.cap1 = cap1;
