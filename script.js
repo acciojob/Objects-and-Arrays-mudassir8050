fb4ec92efdce86a2bafdc8bbac71afe0645c8908
mudassir8050 <125982472+mudassir8050@users.noreply.github.com>
2023-06-15 12:58:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-var players = ["John", "Bob", "Alice", "Poppy"];
+# Given array and object
+players = ["John", "Mike", "Sarah"]
+person = {"name": "Alice", "age": 25}
 
-var person = {
-  name: "John Doe",
-  age: 80,	
-};
-var team = players
-var team1 = players
-var cap1 = person
-window.players = players;
-window.person = person;
-window.team = team;
-window.team1 = team1;
-window.cap1 = cap1;
+# Create a reference to players array
+team = players
+
+# Create a copy of players array
+team1 = players.copy()
+
+# Create a copy of person object
+cap1 = person.copy()
+
+# Print the variables
+print("team:", team)
+print("team1:", team1)
+print("cap1:", cap1)
